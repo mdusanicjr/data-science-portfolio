aa7311d28a65dc924f37e167e425031b53b9f313
mdusanicjr <mdusanic@outlook.com>
2026-09-15 10:43:44 -0700

    Modify greet_user to include NFL team responses
    Updated greeting logic to ask for favorite NFL team and respond accordingly.

--- data-science-profession/projects/greeting-project/hello.py
+++ data-science-profession/projects/greeting-project/hello.py
@@ -1,6 +1,18 @@
 # My first Python project in GitHub
+# Orginal code:
+# def greet_user():
+# name = input("Enter your name: ")
+# print(f"Hello, {name}! Welcome to Data Science.")
+# if __name__ == "__main__":
+# greet_user()
+
 def greet_user():
-name = input("Enter your name: ")
-print(f"Hello, {name}! Welcome to Data Science.")
+name = input("Enter your favorite NFL team")
+if name == "49ers" or "Niners" in name:
+print("Bang Bang Niner Gang")
+elif "Rams" in name:
+print("Same old sorry ass Lambs")
+else:
+print("My favorite team is better than your favorite team")
 if __name__ == "__main__":
 greet_user()
